42c98f3d7d7c9d5a200c208962719c7a5735d2e2
Hrittik Bhattacharjee <hrittikcse@outlook.com>
2019-11-28 02:03:57 +0530

    final commit

--- queries.js
+++ queries.js
@@ -10,6 +10,10 @@ const pool = new Pool({
 	password: 'password',
 	port: 5432
 });
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: true
+// })
 
 const getBanks = (req, res) => {
 	pool.query(`SELECT * FROM banks`, (err, result) => {
